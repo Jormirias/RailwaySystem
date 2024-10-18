9d07aeaa23f4aa3a525540dc270336e5dbbe285f
Jormirias <jormirias@gmail.com>
2024-10-18 20:07:20 +0100

    Partially complete DoubleList struct - still missing some TODOs

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+/**
+ * Sistema de Suporte de uma Rede Ferroviária
+ * @author Jorge Dias (72360) jmr.dias@alumni.fct.unl.pt
+ * @author NOMEALUNO2 (NUMEROALUNO2) emailALUNO2
+ * @version 1.0
+ *
+ */
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
